c588bbfe00f040ce98ff9cfe216f8a28594ff78b
Andrey Kalibin <57500032+Andrey2709@users.noreply.github.com>
2021-07-10 21:01:29 +0300

    поменял протектед на приват

--- src/HomeWorkApp.java
+++ src/HomeWorkApp.java
@@ -10,23 +10,23 @@ public class HomeWorkApp {
         printColor();
         compareNumbers();
     }
-    protected static void printThreeWords(){
+    private static void printThreeWords(){
         System.out.println(" Orange \n Banana \n Apple");
     }
-    protected static void  checkSumSign(){
+    private static void  checkSumSign(){
         int a =3 ,b = 5;
         int sum = a + b;
         if(sum>=0) System.out.println("Сумма положительная");
         else System.out.println("Сумма отрицательная");
     }
 
-    protected static void  printColor(){
+    private static void  printColor(){
         int value = 101;
         if(value<=0){System.out.println("Красный"); }
         else if(value > 0 && value<=100){System.out.println("Жёлтый"); }
         else if(value>100){System.out.println("Зелёный"); }
     }
-    protected static void compareNumbers(){
+    private static void compareNumbers(){
         int a = 10, b =8;
         if(a>=b) System.out.println("a>=b");
         else System.out.println("a<b");
